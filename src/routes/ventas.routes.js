9e15f9f0b234373c7d8724f809d6a053f5c59fe5
CristhelSoza03 <rossemarys003@gmail.com>
2025-05-04 19:58:43 -0600

    banckend corregido.

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import {  obtenerVentasConDetalles} from '../controllers/ventas.controller.js';
+import { obtenerVentas } from '../controllers/Ventasd.controller.js';
 
 const router = Router();
 
 // Ruta para obtener todas las ventas
 router.get('/ventas', obtenerVentasConDetalles);
+
+// Ruta para obtener todas las ventas
+router.get('/obtenerventas', obtenerVentas);
 
 export default router;
